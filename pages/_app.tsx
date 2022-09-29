8fc7745a52369d5d496469157255e16dcf40b67a
Ekene Nwobodo <nwobodoe71@gmail.com>
2022-09-29 09:01:55 +0100

    Add default css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,25 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { wrapper } from "../store";
 import { Provider } from "react-redux";
+import GlobalStyle from "../theme/globalStyles";
+import { ThemeProvider } from "styled-components";
+
+import Theme from "../theme/theme";
+import { StyledContainer } from "../theme/styledContainer";
 
 function MyApp({ Component, ...pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
   return (
     <Provider store={store}>
+       <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <StyledContainer>
       <Component {...props.pageProps} />
+      </StyledContainer>
+      </ThemeProvider>
     </Provider>
   );
 }
+
 
 export default MyApp;
---
+++ theme/styledContainer.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Poppins:wght@400;500;600;700;800&display=swap');
+`
