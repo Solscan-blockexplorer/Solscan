c9a807ef5f155a774dd12afb619dd2174dad8e99
Ekene Nwobodo <nwobodoe71@gmail.com>
2022-09-29 14:31:44 +0100

    Update index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,20 +6,22 @@ import { ThemeProvider } from "styled-components";
 
 import Theme from "../theme/theme";
 import { StyledContainer } from "../theme/styledContainer";
+import Layout from "../components/layout";
 
 function MyApp({ Component, ...pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
   return (
     <Provider store={store}>
-       <ThemeProvider theme={Theme}>
-      <GlobalStyle />
-      <StyledContainer>
-      <Component {...props.pageProps} />
-      </StyledContainer>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <StyledContainer>
+          <Layout>
+            <Component {...props.pageProps} />
+          </Layout>
+        </StyledContainer>
       </ThemeProvider>
     </Provider>
   );
 }
-
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
+import Meta from '../components/Meta'
+import SearchBox from '../components/search'
 
 
 const Home: NextPage = () => {
   return (
-      <h1>j</h1>
+    <>
+      
+      </>
   )
 }
 
