2f93f11284089ccfa09b5a044bd31287e1f7746f
Ekene Nwobodo <nwobodoe71@gmail.com>
2022-10-24 16:25:23 +0100

    Add reload on wallet Search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from 'next'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import UserTable from '../components/UserTable/UserTable'
 import Overview from '../components/wallet-overview'
 import { useAppDispatch, useAppSelector } from '../hooks/setup'
+import { WalletTransaction } from '../interface'
 import { getBalance, getTransactions } from '../store/features/wallet/walletAsyncThunkActions'
-import { handleAddressChange, handleLocalStorage, wallet } from '../store/features/wallet/walletSlice'
+import { CurrentWalletTransaction, handleAddressChange, handleLocalStorage, wallet } from '../store/features/wallet/walletSlice'
 
 
 const Home: NextPage = () => {
   const dispatch = useAppDispatch();
   const {isLoaded,isSuccess,message,balance,validatedAddress,walletTransactions} = useAppSelector(wallet);
+  const [transactions, setTransactions] = useState<CurrentWalletTransaction[] | null>(null) 
+
 
   useEffect(()=>{
     const address = localStorage.getItem('validatedAddress')
@@ -18,14 +21,15 @@ const Home: NextPage = () => {
      dispatch(getTransactions(validAddress))
      dispatch(handleLocalStorage(validAddress))
       dispatch(getBalance(validAddress))
+      setTransactions(walletTransactions)
     }
-  },[])
+  },[transactions])
 
 
   return (
     <>
     <Overview walletAddress={validatedAddress} walletAmount={balance}/>
-    <UserTable walletTransactions={walletTransactions} isLoaded={isLoaded}/>
+    <UserTable walletTransactions={transactions} isLoaded={isLoaded}/>
     </>
   )
 }
